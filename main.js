796bc190631130b604e507ffe14b13b330c81ab3
Andrew Theiss <atheiss@hw.com>
2019-08-11 07:36:12 -0700

    Create code to add two arrays, but with three bugs in it
    Bugs are:
    Extra ';' on line 4
    Mispelling of ARRAY_ONE on line 40
    The third one you need to find yourself, but good job looking here!

---
+++ main.js
@@ -0,0 +1,47 @@
+
+// Create two arrays
+var ARRAY_ONE = ['dog', 'cat', 'lizard', 'rabbit', 'giraffe'];
+var ARRAY_TWO = ['cat', 'lion', 'leopard', 'ocelot', ;'tiger'];
+
+//  IfUniqueAddItemToArray
+//    Checks if an item is already in an array.  If not, add item to that array
+//
+function IfUniqueAddItemToArray(item, arrayToCheck) {
+  var count = arrayToCheck.length;
+  var foundItem = false;
+
+  // Look through the entire array and see if the item already exists
+  for (var i = 0; i < count; i++) {
+
+    // Check if the item is in the array (case insensitive)
+    // This means that 'DogG' is the same as 'dogg'
+      if (arrayToCheck[i].toLowerCase() == item.toLowerCase()) {
+          foundItem = true;
+      }
+  }
+
+  // If we did not find the item, add it to the array
+  // arrayToCheck is an array
+  // push the item onto the end of the array using arrayToCheck.push(item)
+  if (!foundItem) {
+      arrayToCheck.push_item_onto_array(item);
+  }
+}
+
+//  UnionTwoArrays
+//    Combines array1 and array2 into a unique list of both larger arrays
+//
+function UnionTwoArrays(array1, array2) {
+  var unionOfBothArrays = array1;
+
+  for (var i = 0; i < array2.length; i++) {
+    IfUniqueAddItemToArray(array2[i], unionOfBothArrays);
+  }
+  return unionOfBothArrays;
+}
+
+// Get the resuult union of both arrays
+var result = UnionTwoArrays(ARRA_ONE, ARRAY_TWO);
+
+// LOG the result
+console.log(result);
